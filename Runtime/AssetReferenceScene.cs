059afd78b31bcd9b3db6e7878a03d2571db73a5d
Marc-Antoine Girard <m.antoine.girard@gmail.com>
2022-08-11 16:18:30 -0400

    Added assemblies

---
+++ Runtime/AssetReferenceScene.cs
@@ -0,0 +1,23 @@
+﻿#if ADDRESSABLES
+using System;
+using UnityEngine.AddressableAssets;
+
+namespace ShackLab
+{
+#if UNITY_EDITOR
+    using UnityEditor;
+
+    [Serializable]
+    public class AssetReferenceScene : AssetReferenceT<SceneAsset>
+    {
+        public AssetReferenceScene(string guid) : base(guid) { }
+    }
+#else
+    [Serializable]
+    public class AssetReferenceScene : AssetReference
+    {
+        public AssetReferenceScene(string guid) : base(guid) {}
+    }
+#endif
+}
+#endif
